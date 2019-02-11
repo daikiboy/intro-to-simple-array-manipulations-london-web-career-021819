34e9c08b6c68c60fb87b33249b4749c788f61049
simone.rainieri <simone.rainieri@gmail.com>
2019-02-11 17:48:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  next_country = "Niger"
+  countries_in_western_africa.push(next_country)
+  return countries_in_western_africa
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+end
+
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties.pop()
+end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
+
+def using_unshift(my_favorite_cities)
+  my_favorite_cities.shift()
+end
+
+
+  
